43d8bedd1253e1e0cac1270b496dac69735a1dbc
24wings <2121718893@qq.com>
2017-01-17 10:24:36 +0800

    app.ts

--- nodejs/demos/demo1/app.ts
+++ nodejs/demos/demo1/app.ts
@@ -1,5 +1,13 @@
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
+
 var app =express();
+
+
+    
+// 解析请求体
+app.use(bodyParser());
+app.use(bodyParser.urlencoded({extended:true}));
 
 
 // 设置视图存放的文件夹
@@ -8,11 +16,15 @@ app.set('views',__dirname+'/views')
 //  视图的渲染引擎
 app.set('view engine','ejs');
 
+
 // 对于任何请求渲染index页面
-app.all('*',(req,res,next)=>{
+app.all('/',(req,res,next)=>{
     //渲染并发送渲染后的页面
-res.render('index',{username:'杨杰'});
+    
+res.render('index',{username:req.body.username});
 });
+
+
 
 
 
